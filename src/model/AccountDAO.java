70522b9636521b951ae7b28a4de8e5b2814373e2
Nguyen Minh Quan <quannguyenminh1001@gmail.com>
2022-10-25 01:11:02 +0700

    create method getPasswordEncryption

--- src/model/AccountDAO.java
+++ src/model/AccountDAO.java
@@ -3,9 +3,9 @@ package model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 
-import entity.Account;
-import entity.Department;
 
 public class AccountDAO {
 	
@@ -28,4 +28,21 @@ public class AccountDAO {
 //			e.printStackTrace();
 //		}
 //	}
+	
+	public HashMap<String, String> getPasswordEncryption(String username) {
+		HashMap<String, String> account = new HashMap<String, String>();
+		try {
+			PreparedStatement stmt = connection.prepareStatement("SELECT MatKhau, GiaTriSalt FROM TAIKHOAN WHERE TenDangNhap = ?");
+			stmt.setString(1, username);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				account.put("username", username);
+				account.put("password hash", rs.getString("MatKhau"));
+				account.put("salt", rs.getString("GiaTriSalt"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return account;
+	}
 }
