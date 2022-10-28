1739093f3fbbde1c4f7691352d46201b274c30c8
Nguyen Minh Quan <quannguyenminh1001@gmail.com>
2022-10-29 01:23:49 +0700

    create Application.java

---
+++ src/ui/Application.java
@@ -0,0 +1,21 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.UIManager;
+
+public class Application {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+					LoginGUI frame = new LoginGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
